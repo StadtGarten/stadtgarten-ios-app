dfa2ca3e9dcde43c10fb7ad2ce6803b97c673b41
Dennis Schaaf <Dennis.Schaaf@frogdesign.com>
2014-05-07 13:51:19 +0200

    Initial Commit

---
+++ StadtgartenApp/SGAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SGAppDelegate.h
+//  StadtgartenApp
+//
+//  Created by Dennis Schaaf on 07/05/14.
+//  Copyright (c) 2014 StadtGarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ StadtgartenApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StadtgartenApp
+//
+//  Created by Dennis Schaaf on 07/05/14.
+//  Copyright (c) 2014 StadtGarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGAppDelegate class]));
+    }
+}
