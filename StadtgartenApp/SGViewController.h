dbc278b0d77c06ea9952804833634b4958a539c3
Tobias Keinath <keinath.t@googlemail.com>
2014-05-14 13:29:35 +0200

    Storyboard added

---
+++ StadtgartenApp/SGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGViewController.h
+//  StadtgartenApp
+//
+//  Created by Tobias Keinath on 13.05.14.
+//  Copyright (c) 2014 StadtGarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGViewController : UIViewController
+
+@end
