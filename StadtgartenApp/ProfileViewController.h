10897294d458b246620ebe8b000f18b9d24640c8
Tobias Keinath <keinath.t@googlemail.com>
2014-06-11 12:46:35 +0200

    ProfileView Project Settings

---
+++ StadtgartenApp/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  StadtgartenApp
+//
+//  Created by Tobias Keinath on 11.06.14.
+//  Copyright (c) 2014 StadtGarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface ProfileViewController : UIViewController
+@property (strong, nonatomic) IBOutlet FBProfilePictureView *profilePicture;
+@property (strong, nonatomic) IBOutlet UILabel *lblUsername;
+@property (strong, nonatomic) IBOutlet UILabel *lblEmail;
+
+@end
