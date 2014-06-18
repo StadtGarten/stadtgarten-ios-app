52ab3ffa984ed3a6e5fdb4ab8014a0cefb0b92a0
Tobias Keinath <keinath.t@googlemail.com>
2014-06-18 11:53:06 +0200

    TreeDetailsView added, editing

---
+++ TreeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TreeDetailViewController.h
+//  StadtgartenApp
+//
+//  Created by Tobias Keinath on 17.06.14.
+//  Copyright (c) 2014 StadtGarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TreeDetailViewController : UIViewController
+
+@end
