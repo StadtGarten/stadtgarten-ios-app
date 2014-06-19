0314ffbb2b0934011b50e9c822a0e45d82f7d418
Tobias Keinath <keinath.t@googlemail.com>
2014-06-19 13:11:31 +0200

    fb

--- StadtgartenApp/SGAppDelegate.h
+++ StadtgartenApp/SGAppDelegate.h
@@ -17,7 +17,7 @@
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
-q
+@property (strong, nonatomic) FBSession *session;
 
 - (BOOL)openSessionWithAllowLoginUI:(BOOL)allowLoginUI;
 - (void) checkSessionState:(FBSessionState)state;
