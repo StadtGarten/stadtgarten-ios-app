ef4268963e40f7b42642371dcbef4eb2d81a57e1
admin <admin@admins-MacBook-Pro-3.local>
2014-06-19 09:18:38 +0200

    abfragefunktion für tree Ratings hinzugefügt

--- Database.h
+++ Database.h
@@ -13,7 +13,7 @@
 -(void)writeTree:(NSString*)userid baumname:(NSString*)baumname tag:(NSString*)tag beschreibung:(NSString*)beschreibung bild:(UIImage*)bild;
 -(NSArray *)getTrees;
 -(NSArray *)getUserTrees:(NSString*)userid;
--(float)getTreeRating:(NSString*)treeid;
--(void)rateTree:(NSString*)userid treeid:(NSString*)treeid rating:(float)rating;
+-(NSNumber*)getTreeRating:(NSString*)treeid;
+-(void)rateTree:(NSString*)userid treeid:(NSString*)treeid rating:(NSNumber*)rating;
 
 @end
